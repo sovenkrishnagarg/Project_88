22c5486a7d775b916be47b1126d3f8d5a8d1bcad
sovenkrishnagarg <117274584+sovenkrishnagarg@users.noreply.github.com>
2024-01-15 12:26:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ block_image_height = 5;
 
 function load_img(){
 	// write code to Upload golf image on the canvas
-	fabric.Image.fromUrl("golf-h.png", function(Img){
+	fabric.Image.fromURl("golf-h.png", function(Img){
 		hole_obj = Img;
 		hole_obj.scaleToWidth(50);
 		hole_obj.scaleToHeight(50);
@@ -29,7 +29,7 @@ function load_img(){
 
 function new_image(){
 	// write code to Upload ball image on canvas
-	fabric.Image.fromUrl("ball.png", function(Img){
+	fabric.Image.fromURL("ball.png", function(Img){
 		ball_obj = Img;
 		ball_obj.scaleToWidth(50);
 		ball_obj.scaleToHeight(50);
